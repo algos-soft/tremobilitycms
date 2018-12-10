81eeeb971c9f90b81bc6167e99fef664132f5cbc
Daniele Marabese <daniele.marabese@altran.com>
2018-12-10 16:23:57 +0100

    Added handling of cards with label in cardview as in zoneitemsview

--- src/main/java/it/windtre/tremobilitycms/backend/repositories/ElementRepository.java
+++ src/main/java/it/windtre/tremobilitycms/backend/repositories/ElementRepository.java
@@ -1,11 +1,13 @@
 package it.windtre.tremobilitycms.backend.repositories;
 
 import it.windtre.tremobilitycms.backend.data.entity.Element;
+import it.windtre.tremobilitycms.backend.data.entity.Serviceitem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface ElementRepository extends JpaRepository<Element, Long> {
@@ -17,4 +19,7 @@ public interface ElementRepository extends JpaRepository<Element, Long> {
     //int countByNameLikeIgnoreCase(String name);
 
     List<Element> findAll();
+
+    Optional<Element> findById(Long id);
+
 }
