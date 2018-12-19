bb2196af5e40a24e5f3d3fb85116d1fb36ef915f
Francesco Orzali <francesco.orzali@windtre.it>
2018-12-19 14:02:17 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export _JAVA_OPTIONS="$_JAVA_OPTIONS -Dssl.keystore=${WIDI_TOMCAT_SSL_CERT}"
+export _JAVA_OPTIONS="$_JAVA_OPTIONS -Dssl.keystore=${WIDI_TOMCAT_SSL_CERT} -Dwidi.port=${WIDI_HTTP_PORT}"
 
 catalina.sh run
 
